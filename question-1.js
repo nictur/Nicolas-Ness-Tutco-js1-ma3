c1ce3e28ff36b59b50018ab03dc80d0cdc518524
nictur <112319517+nictur@users.noreply.github.com>
2023-05-14 19:52:42 +0200

    question 1
    awsner

---
+++ question-1.js
@@ -0,0 +1,14 @@
+// "old function"
+function getRemainder(a,b) {
+    return a % b;
+    }
+
+
+// converted function
+
+const getRemainder = (a, b) => {
+    return a - b;
+
+}
+
+console.log(getRemainder);
---
+++ question-2.js
@@ -0,0 +1,15 @@
+
+// "old function"
+function getRemainder(a,b) {
+    return a % b;
+    }
+
+
+// converted function
+
+const getRemainder = (a, b) => {
+    return a - b;
+
+}
+
+console.log(getRemainder);
